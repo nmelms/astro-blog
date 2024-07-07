0a4dcce820f90c430cbdf8de91f6ff88eb25e458
Nick Melms <nick@craftedagency.com>
2024-07-07 16:05:59 -0400

    restyle inner borders unsing puesdo element

--- src/lib/fetchRemainingBlogs.js
+++ src/lib/fetchRemainingBlogs.js
@@ -1,6 +1,6 @@
 export async function fetchRemainingBlogs() {
   const response = await fetch(
-    "http://localhost:3000/api/blogs?sort=-createdAt&offset=3"
+    "http://localhost:3000/api/blogs?sort[createdAt]=-1&offset=3"
   );
   const data = await response.json();
   return data;
