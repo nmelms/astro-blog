502482f88815d7b14ac3a0332db3a1d4129d99e3
Nick Melms <nick@craftedagency.com>
2024-07-19 22:47:21 -0400

    dynamicaly reate robots.txt and sitemap with astro functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  site: "https://blog.nickmelms.dev",
   css: {
     files: ["src/styles/globals.css"],
   },
